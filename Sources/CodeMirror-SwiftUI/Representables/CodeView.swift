a6de92b75ffef435a1bf5e9c209237695d8c44f9
Yuexie <yuexie.zhou@thoughtworks.com>
2022-04-05 12:19:17 +0800

    make code mode identity

--- Sources/CodeMirror-SwiftUI/Representables/CodeView.swift
+++ Sources/CodeMirror-SwiftUI/Representables/CodeView.swift
@@ -22,63 +22,63 @@ typealias RepresentableView = UIViewRepresentable
 // MARK: - CodeView
 
 public struct CodeView: RepresentableView {
-
-  @Binding var code: String
-  
-  var theme: CodeViewTheme
-  var mode: Mode
-  var fontSize: Int
-  var showInvisibleCharacters: Bool
-  var lineWrapping: Bool
-  
-  var onLoadSuccess: (() -> ())?
-  var onLoadFail: ((Error) -> ())?
-  var onContentChange: ((String) -> ())?
-  
-  
-  public init(theme: CodeViewTheme = CodeViewTheme.materialPalenight,
-              code: Binding<String>,
-              mode: Mode,
-              fontSize: Int = 12,
-              showInvisibleCharacters: Bool = true,
-              lineWrapping: Bool = true) {
-    self._code = code
-    self.mode = mode
-    self.theme = theme
-    self.fontSize = fontSize
-    self.showInvisibleCharacters = showInvisibleCharacters
-    self.lineWrapping = lineWrapping
-  }
-  
-  
-  // MARK: - Life Cycle
-  
-  #if os(OSX)
-  public func makeNSView(context: Context) -> WKWebView {
-    createWebView(context)
-  }
-  #elseif os(iOS)
-  public func makeUIView(context: Context) -> WKWebView {
-    createWebView(context)
-  }
-  #endif
-  
-  #if os(OSX)
-  public func updateNSView(_ codeMirrorView: WKWebView, context: Context) {
     
-    updateWebView(context)
-  }
-  #elseif os(iOS)
-  public func updateUIView(_ uiView: WKWebView, context: Context) {
+    @Binding var code: String
     
-    updateWebView(context)
-  }
-  #endif
-  
-  public func makeCoordinator() -> CodeViewController {
-    CodeViewController(self)
-  }
-  
+    var theme: CodeViewTheme
+    var mode: Mode
+    var fontSize: Int
+    var showInvisibleCharacters: Bool
+    var lineWrapping: Bool
+    
+    var onLoadSuccess: (() -> ())?
+    var onLoadFail: ((Error) -> ())?
+    var onContentChange: ((String) -> ())?
+    
+    
+    public init(theme: CodeViewTheme = CodeViewTheme.materialPalenight,
+                code: Binding<String>,
+                mode: Mode,
+                fontSize: Int = 12,
+                showInvisibleCharacters: Bool = true,
+                lineWrapping: Bool = true) {
+        self._code = code
+        self.mode = mode
+        self.theme = theme
+        self.fontSize = fontSize
+        self.showInvisibleCharacters = showInvisibleCharacters
+        self.lineWrapping = lineWrapping
+    }
+    
+    
+    // MARK: - Life Cycle
+    
+#if os(OSX)
+    public func makeNSView(context: Context) -> WKWebView {
+        createWebView(context)
+    }
+#elseif os(iOS)
+    public func makeUIView(context: Context) -> WKWebView {
+        createWebView(context)
+    }
+#endif
+    
+#if os(OSX)
+    public func updateNSView(_ codeMirrorView: WKWebView, context: Context) {
+        
+        updateWebView(context)
+    }
+#elseif os(iOS)
+    public func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+        updateWebView(context)
+    }
+#endif
+    
+    public func makeCoordinator() -> CodeViewController {
+        CodeViewController(self)
+    }
+    
 }
 
 
@@ -86,24 +86,21 @@ public struct CodeView: RepresentableView {
 // MARK: - Public API
 
 extension CodeView {
-  
-  public func onLoadSuccess(_ action: @escaping (() -> ())) -> Self {
-    var copy = self
-    copy.onLoadSuccess = action
-    return copy
-  }
-  
-  public func onLoadFail(_ action: @escaping ((Error) -> ())) -> Self {
-    var copy = self
-    copy.onLoadFail = action
-    return copy
-  }
-  
-  public func onContentChange(_ action: @escaping ((String) -> ())) -> Self {
-    var copy = self
-    copy.onContentChange = action
-    return copy
-  }
+    
+    public mutating func onLoadSuccess(_ action: @escaping (() -> ())) -> Self {
+        self.onLoadSuccess = action
+        return self
+    }
+    
+    public mutating func onLoadFail(_ action: @escaping ((Error) -> ())) -> Self {
+        self.onLoadFail = action
+        return self
+    }
+    
+    public mutating func onContentChange(_ action: @escaping ((String) -> ())) -> Self {
+        self.onContentChange = action
+        return self
+    }
 }
 
 
@@ -111,79 +108,79 @@ extension CodeView {
 // MARK: - Private API
 
 extension CodeView {
-  
-  private func createWebView(_ context: Context) -> WKWebView {
-    let preferences = WKPreferences()
-    preferences.javaScriptEnabled = true
     
-    let userController = WKUserContentController()
-    userController.add(context.coordinator, name: CodeViewRPC.isReady)
-    userController.add(context.coordinator, name: CodeViewRPC.textContentDidChange)
-    
-    let configuration = WKWebViewConfiguration()
-    configuration.preferences = preferences
-    configuration.userContentController = userController
-    
-    let webView = WKWebView(frame: .zero, configuration: configuration)
-    webView.navigationDelegate = context.coordinator
-    #if os(OSX)
-    webView.setValue(true, forKey: "drawsTransparentBackground")
-    webView.allowsMagnification = false
-    #elseif os(iOS)
-    webView.isOpaque = false
-    #endif
-    
-    let codeMirrorBundle = try! Bundle.codeMirrorBundle()
-    guard let indexPath = codeMirrorBundle.path(forResource: "index", ofType: "html") else {
-      fatalError("CodeMirrorBundle is missing")
-    }
-    
-    let data = try! Data(contentsOf: URL(fileURLWithPath: indexPath))
-    
-    webView.load(data, mimeType: "text/html", characterEncodingName: "utf-8", baseURL: codeMirrorBundle.resourceURL!)
-
-    context.coordinator.setWebView(webView)
-    context.coordinator.setThemeName(theme.rawValue)
-    
-    context.coordinator.setMimeType(mode.mimeType)
-    context.coordinator.setContent(code)
-    context.coordinator.setFontSize(fontSize)
-    context.coordinator.setShowInvisibleCharacters(showInvisibleCharacters)
-    context.coordinator.setLineWrapping(lineWrapping)
-    
-    return webView
-  }
-  
-  fileprivate func updateWebView(_ context: CodeView.Context) {
-    updateWhatsNecessary(elementGetter: context.coordinator.getMimeType(_:), elementSetter: context.coordinator.setMimeType(_:), currentElementState: self.mode.mimeType)
-    
-    updateWhatsNecessary(elementGetter: context.coordinator.getContent(_:), elementSetter: context.coordinator.setContent(_:), currentElementState: self.code)
-    
-    context.coordinator.setThemeName(self.theme.rawValue)
-    context.coordinator.setFontSize(fontSize)
-    context.coordinator.setShowInvisibleCharacters(showInvisibleCharacters)
-    context.coordinator.setLineWrapping(lineWrapping)
-  }
-  
-  func updateWhatsNecessary(elementGetter: (JavascriptCallback?) -> Void,
-                            elementSetter: @escaping (_ elementState: String) -> Void,
-                            currentElementState: String) {
-    elementGetter({ result in
-      
-      switch result {
-      case .success(let resp):
-        guard let previousElementState = resp as? String else { return }
+    private func createWebView(_ context: Context) -> WKWebView {
+        let preferences = WKPreferences()
+        preferences.javaScriptEnabled = true
         
-        if previousElementState != currentElementState {
-          elementSetter(currentElementState)
+        let userController = WKUserContentController()
+        userController.add(context.coordinator, name: CodeViewRPC.isReady)
+        userController.add(context.coordinator, name: CodeViewRPC.textContentDidChange)
+        
+        let configuration = WKWebViewConfiguration()
+        configuration.preferences = preferences
+        configuration.userContentController = userController
+        
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.navigationDelegate = context.coordinator
+#if os(OSX)
+        webView.setValue(true, forKey: "drawsTransparentBackground")
+        webView.allowsMagnification = false
+#elseif os(iOS)
+        webView.isOpaque = false
+#endif
+        
+        let codeMirrorBundle = try! Bundle.codeMirrorBundle()
+        guard let indexPath = codeMirrorBundle.path(forResource: "index", ofType: "html") else {
+            fatalError("CodeMirrorBundle is missing")
         }
         
-        return
-      case .failure(let error):
-        print("Error \(error)")
-        return
-      }
-    })
-  }
-  
+        let data = try! Data(contentsOf: URL(fileURLWithPath: indexPath))
+        
+        webView.load(data, mimeType: "text/html", characterEncodingName: "utf-8", baseURL: codeMirrorBundle.resourceURL!)
+        
+        context.coordinator.setWebView(webView)
+        context.coordinator.setThemeName(theme.rawValue)
+        
+        context.coordinator.setMimeType(mode.mimeType)
+        context.coordinator.setContent(code)
+        context.coordinator.setFontSize(fontSize)
+        context.coordinator.setShowInvisibleCharacters(showInvisibleCharacters)
+        context.coordinator.setLineWrapping(lineWrapping)
+        
+        return webView
+    }
+    
+    fileprivate func updateWebView(_ context: CodeView.Context) {
+        updateWhatsNecessary(elementGetter: context.coordinator.getMimeType(_:), elementSetter: context.coordinator.setMimeType(_:), currentElementState: self.mode.mimeType)
+        
+        updateWhatsNecessary(elementGetter: context.coordinator.getContent(_:), elementSetter: context.coordinator.setContent(_:), currentElementState: self.code)
+        
+        context.coordinator.setThemeName(self.theme.rawValue)
+        context.coordinator.setFontSize(fontSize)
+        context.coordinator.setShowInvisibleCharacters(showInvisibleCharacters)
+        context.coordinator.setLineWrapping(lineWrapping)
+    }
+    
+    func updateWhatsNecessary(elementGetter: (JavascriptCallback?) -> Void,
+                              elementSetter: @escaping (_ elementState: String) -> Void,
+                              currentElementState: String) {
+        elementGetter({ result in
+            
+            switch result {
+            case .success(let resp):
+                guard let previousElementState = resp as? String else { return }
+                
+                if previousElementState != currentElementState {
+                    elementSetter(currentElementState)
+                }
+                
+                return
+            case .failure(let error):
+                print("Error \(error)")
+                return
+            }
+        })
+    }
+    
 }
--- Sources/CodeMirror-SwiftUI/Representables/CodeViewController.swift
+++ Sources/CodeMirror-SwiftUI/Representables/CodeViewController.swift
@@ -15,14 +15,14 @@ import WebKit
 
 typealias JavascriptCallback = (Result<Any?, Error>) -> Void
 private struct JavascriptFunction {
-  
-  let functionString: String
-  let callback: JavascriptCallback?
-  
-  init(functionString: String, callback: JavascriptCallback? = nil) {
-    self.functionString = functionString
-    self.callback = callback
-  }
+    
+    let functionString: String
+    let callback: JavascriptCallback?
+    
+    init(functionString: String, callback: JavascriptCallback? = nil) {
+        self.functionString = functionString
+        self.callback = callback
+    }
 }
 
 
@@ -30,50 +30,50 @@ private struct JavascriptFunction {
 // MARK: - CodeMirrorViewController Coordinator
 
 public class CodeViewController: NSObject {
-  
-  // MARK: Properties
-  
-  var parent: CodeView
-  weak var webView: WKWebView?
-  
-  fileprivate var pageLoaded = false
-  fileprivate var pendingFunctions = [JavascriptFunction]()
-  
-  init(_ parent: CodeView) {
-    self.parent = parent
-  }
-  
-  private func addFunction(function:JavascriptFunction) {
-    pendingFunctions.append(function)
-  }
-  
-  private func callJavascriptFunction(function: JavascriptFunction) {
-    webView?.evaluateJavaScript(function.functionString) { (response, error) in
-      if let error = error {
-        function.callback?(.failure(error))
-      }
-      else {
-        function.callback?(.success(response))
-      }
+    
+    // MARK: Properties
+    
+    var parent: CodeView
+    weak var webView: WKWebView?
+    
+    fileprivate var pageLoaded = false
+    fileprivate var pendingFunctions = [JavascriptFunction]()
+    
+    init(_ parent: CodeView) {
+        self.parent = parent
     }
-  }
-  
-  private func callPendingFunctions() {
-    for function in pendingFunctions {
-      callJavascriptFunction(function: function)
+    
+    private func addFunction(function:JavascriptFunction) {
+        pendingFunctions.append(function)
     }
-    pendingFunctions.removeAll()
-  }
-  
-  private func callJavascript(javascriptString: String, callback: JavascriptCallback? = nil) {
-    if pageLoaded {
-      callJavascriptFunction(function: JavascriptFunction(functionString: javascriptString, callback: callback))
+    
+    private func callJavascriptFunction(function: JavascriptFunction) {
+        webView?.evaluateJavaScript(function.functionString) { (response, error) in
+            if let error = error {
+                function.callback?(.failure(error))
+            }
+            else {
+                function.callback?(.success(response))
+            }
+        }
     }
-    else {
-      addFunction(function: JavascriptFunction(functionString: javascriptString, callback: callback))
+    
+    private func callPendingFunctions() {
+        for function in pendingFunctions {
+            callJavascriptFunction(function: function)
+        }
+        pendingFunctions.removeAll()
     }
-  }
-  
+    
+    private func callJavascript(javascriptString: String, callback: JavascriptCallback? = nil) {
+        if pageLoaded {
+            callJavascriptFunction(function: JavascriptFunction(functionString: javascriptString, callback: callback))
+        }
+        else {
+            addFunction(function: JavascriptFunction(functionString: javascriptString, callback: callback))
+        }
+    }
+    
 }
 
 
@@ -81,21 +81,21 @@ public class CodeViewController: NSObject {
 // MARK: - WKScriptMessageHandler Implementation
 
 extension CodeViewController: WKScriptMessageHandler {
-  
-  public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-    print("didFinish")
-    parent.onLoadSuccess?()
-  }
-  
-  public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-    print("didFail \(error.localizedDescription)")
-    parent.onLoadFail?(error)
-  }
-  
-  public func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-    print("didFailProvisionalNavigation")
-    parent.onLoadFail?(error)
-  }
+    
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        debugPrint("didFinish")
+        parent.onLoadSuccess?()
+    }
+    
+    public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        debugPrint("didFail \(error.localizedDescription)")
+        parent.onLoadFail?(error)
+    }
+    
+    public func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        debugPrint("didFailProvisionalNavigation")
+        parent.onLoadFail?(error)
+    }
 }
 
 
@@ -103,28 +103,28 @@ extension CodeViewController: WKScriptMessageHandler {
 // MARK: - WKNavigationDelegate Implementation
 
 extension CodeViewController: WKNavigationDelegate {
-  
-  public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
     
-    // is Ready
-    if message.name == CodeViewRPC.isReady {
-      pageLoaded = true
-      callPendingFunctions()
-      return
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
+        // is Ready
+        if message.name == CodeViewRPC.isReady {
+            pageLoaded = true
+            callPendingFunctions()
+            return
+        }
+        
+        // Content change
+        if message.name == CodeViewRPC.textContentDidChange {
+            let content = (message.body as? String) ?? ""
+            
+            if content != parent.code {
+                parent.onContentChange?(content)
+                parent.code = content
+            }
+            return
+        }
     }
     
-    // Content change
-    if message.name == CodeViewRPC.textContentDidChange {
-      let content = (message.body as? String) ?? ""
-      
-      if content != parent.code {
-        parent.onContentChange?(content)
-        parent.code = content
-      }
-      return
-    }
-  }
-  
 }
 
 
@@ -132,64 +132,64 @@ extension CodeViewController: WKNavigationDelegate {
 // MARK: - Public API
 
 extension CodeViewController {
-  
-  public func setWebView(_ webView: WKWebView) {
-    self.webView = webView
-    setDefaultTheme()
-    setTabInsertsSpaces(true)
-  }
-  
-  func setTabInsertsSpaces(_ value: Bool) {
-    callJavascript(javascriptString: "SetTabInsertSpaces(\(value));")
-  }
-  
-  func setContent(_ value: String) {
-    if let hexString = value.data(using: .utf8)?.hexEncodedString() {
-      let script = """
+    
+    public func setWebView(_ webView: WKWebView) {
+        self.webView = webView
+        setDefaultTheme()
+        setTabInsertsSpaces(true)
+    }
+    
+    func setTabInsertsSpaces(_ value: Bool) {
+        callJavascript(javascriptString: "SetTabInsertSpaces(\(value));")
+    }
+    
+    func setContent(_ value: String) {
+        if let hexString = value.data(using: .utf8)?.hexEncodedString() {
+            let script = """
       var content = "\(hexString)"; SetContent(content);
       """
-      callJavascript(javascriptString: script)
+            callJavascript(javascriptString: script)
+        }
     }
-  }
-  
-  func getContent(_ block: JavascriptCallback?) {
-    callJavascript(javascriptString: "GetContent();", callback: block)
-  }
-  
-  func setMimeType(_ value: String) {
-    callJavascript(javascriptString: "SetMimeType(\"\(value)\");")
-  }
-  
-  func getMimeType(_ block: JavascriptCallback?) {
-    callJavascript(javascriptString: "GetMimeType();", callback: block)
-  }
-  
-  func setThemeName(_ value: String) {
-    callJavascript(javascriptString: "SetTheme(\"\(value)\");")
-  }
-  
-  func setLineWrapping(_ value: Bool) {
-    callJavascript(javascriptString: "SetLineWrapping(\(value));")
-  }
-  
-  func setFontSize(_ value: Int) {
-    callJavascript(javascriptString: "SetFontSize(\(value));")
-  }
-  
-  func setShowInvisibleCharacters(_ show: Bool) {
-    callJavascript(javascriptString: "ToggleInvisible(\(show));")
-  }
-  
-  func setDefaultTheme() {
-    setMimeType("application/json")
-  }
-  
-  func setReadonly(_ value: Bool) {
-    callJavascript(javascriptString: "SetReadOnly(\(value));")
-  }
-  
-  func getTextSelection(_ block: JavascriptCallback?) {
-    callJavascript(javascriptString: "GetTextSelection();", callback: block)
-  }
-  
+    
+    func getContent(_ block: JavascriptCallback?) {
+        callJavascript(javascriptString: "GetContent();", callback: block)
+    }
+    
+    func setMimeType(_ value: String) {
+        callJavascript(javascriptString: "SetMimeType(\"\(value)\");")
+    }
+    
+    func getMimeType(_ block: JavascriptCallback?) {
+        callJavascript(javascriptString: "GetMimeType();", callback: block)
+    }
+    
+    func setThemeName(_ value: String) {
+        callJavascript(javascriptString: "SetTheme(\"\(value)\");")
+    }
+    
+    func setLineWrapping(_ value: Bool) {
+        callJavascript(javascriptString: "SetLineWrapping(\(value));")
+    }
+    
+    func setFontSize(_ value: Int) {
+        callJavascript(javascriptString: "SetFontSize(\(value));")
+    }
+    
+    func setShowInvisibleCharacters(_ show: Bool) {
+        callJavascript(javascriptString: "ToggleInvisible(\(show));")
+    }
+    
+    func setDefaultTheme() {
+        setMimeType("application/json")
+    }
+    
+    func setReadonly(_ value: Bool) {
+        callJavascript(javascriptString: "SetReadOnly(\(value));")
+    }
+    
+    func getTextSelection(_ block: JavascriptCallback?) {
+        callJavascript(javascriptString: "GetTextSelection();", callback: block)
+    }
+    
 }
